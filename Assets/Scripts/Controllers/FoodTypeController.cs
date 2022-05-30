18328db6ccd4af0a01e47619f966402845570ede
Jacek Woźniak <jac3kw0zniak@gmail.com>
2022-05-30 21:47:31 +0200

    checking strings

--- Assets/Scripts/Controllers/FoodTypeController.cs
+++ Assets/Scripts/Controllers/FoodTypeController.cs
@@ -18,36 +18,30 @@ public partial class FoodTypeController
 
     private FoodType GetClosestFoodTypeAvailable(string name)
     {
+        if (name.Length == 0) return GetRandomFood();
+
         // Performs check for closest representation
         string foodType = _foodTypesNames.FirstOrDefault<string>(x => x == name);
 
         // Performs check for dish that contains parameter
         if (foodType == default)
-        {
             foodType = _foodTypesNames.FirstOrDefault<string>(x => x.Contains(name));
-        }
 
-        // Performs check for dish that at least contains same first 
-        // and second to last letters and have exact same length
-        if (foodType == default)
+        // Naive check solution
+        string temp = name;
+        while (foodType == default && temp.Length > 0)
         {
-            foodType = _foodTypesNames.FirstOrDefault<string>(
-                x => 
-                x[0] == name[0] && x.Length == name.Length && 
-                x[x.Length - 2] == name[name.Length - 2] 
-                );
+            temp = temp.Substring(0, temp.Length - 1);
+            foodType = _foodTypesNames.FirstOrDefault<string>(x => x.Contains(temp));
         }
 
         // Takes random food
-        if (foodType == default)
-        {
-            return GetRandomFood();
-        }
+        if (foodType == default) return GetRandomFood();
 
         return new FoodType(foodType);
     }
 
     private FoodType GetRandomFood() =>
         new FoodType(_foodTypesNames[Random.Range(0, _foodTypesNames.Length)]);
-    
+
 }
